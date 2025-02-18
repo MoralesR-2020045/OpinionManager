bf6786e530664ab26069f08d09b5b189e0266d04
MoralesR-2020045 <rmorales215327@gmail.com>
2025-02-17 19:41:29 -0600

    feat($auth/user): schema creation user

---
+++ src/user/user.models.js
@@ -0,0 +1,59 @@
+import {Schema, model} from "mongoose";
+
+const userSchema = Schema({
+    name:{
+        type: String, 
+        required: [true, "Name is required"],
+        minLength: [3, "Name needs more than 3 characters"],
+        maxLength: [30, "Name cannot exced 30 characters"]
+    },
+    surname:{
+        type: String, 
+        required: [true, "surname is required"],
+        minLength: [3, "Name needs more than 3 characters"],
+        maxLength: [30, "Name cannot exced 30 characters"]
+    },
+    username:{
+        type: String, 
+        unique: true,
+        required: [true, "username is required"],
+        minLength: [3, "Name needs more than 3 characters"],
+        maxLength: [30, "Name cannot exced 30 characters"]
+    },
+    phone:{
+        type: String, 
+        minLength: 8,
+        maxLength: 8,
+    },
+    email:{
+        type: String, 
+        required: [true, "Email is required"],
+        maxLength: [60, "Name cannot exced 30 characters"],
+        unique:true
+    },
+    password:{
+        type: String,
+        required: [true, "Password is required"]
+    },
+    birthdate:{
+        type: Date
+    },
+    gender:{
+        default: "Indefinite"
+    },
+    profilePicture:{
+        type: String
+    },
+    status:{
+        type: Boolean,
+        default: true
+    }
+})
+
+userSchema.methods.toJSON = function(){
+    const {password, _id, ...usuario} = this.toObject()
+    usuario.uid = _id
+    return usuario
+}
+
+export default model ("User", userSchema)
