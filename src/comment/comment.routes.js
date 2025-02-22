f1098a26710b67db204a3e81f734ccae7b250ca6
MoralesR-2020045 <rmorales215327@gmail.com>
2025-02-21 22:50:18 -0600

    feat($src/publicatio): update in bug

--- src/comment/comment.routes.js
+++ src/comment/comment.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { validationAddComment, validationUpdateComment } from "../middleware/comment-validator.js";
-import { addComment, updateComment } from "./comment.controller.js";
+import { validationAddComment, validationUpdateComment, validatorDeleteComment} from "../middleware/comment-validator.js";
+import { addComment, deleteComment, getComment, updateComment } from "./comment.controller.js";
 
 const router = Router()
 
 router.post("/addComment", validationAddComment, addComment);
 router.patch("/updateComment/:uid", validationUpdateComment, updateComment)
+
+router.delete("/deletePublication/:uid", validatorDeleteComment, deleteComment);
+
+router.get("/", getComment);
 
 export default router
--- src/helpers/db-validators.js
+++ src/helpers/db-validators.js
@@ -1,39 +1,47 @@
 import Category from "../category/category.models.js"
 import User from "../user/user.models.js"
 import Publication from "../publication/publication.model.js"
+import Comment from "../comment/comment.model.js"
 
 export const emailExists = async (email = "") => {
-    const existe = await User.findOne({email})
-    if(existe){
+    const existe = await User.findOne({ email })
+    if (existe) {
         throw new Error(`The email ${email} is already registered`)
     }
 }
 
 export const usernameExists = async (username = "") => {
-    const existe = await User.findOne({username})
-    if(existe){
+    const existe = await User.findOne({ username })
+    if (existe) {
         throw new Error(`The username ${username} is already registered`)
     }
 }
 
-export const nameCategoryExists = async (name = "") =>{
-    const exists = await Category.findOne({name})
-    if(exists){
+export const nameCategoryExists = async (name = "") => {
+    const exists = await Category.findOne({ name })
+    if (exists) {
         throw new Error(`The category name exists ${name}`)
     }
 }
 
-export const categoryExist = async (uid = "") =>{
+export const categoryExist = async (uid = "") => {
     const exists = await Category.findById(uid)
-    if(!exists){
+    if (!exists) {
         throw new Error(`The uid has not been found`)
     }
 }
 
-export const  publicationExist = async (uid = "") =>{   
+export const publicationExist = async (uid = "") => {
     const exists = await Publication.findById(uid)
-    if(!exists){
+    if (!exists) {
         throw new Error("Publication does not exist")
     }
 }
 
+
+export const commentExist = async (uid = "") => {
+    const exists = await Comment.findById(uid)
+    if (!exists) {
+        throw new Error("Comment does not exist")
+    }
+}
